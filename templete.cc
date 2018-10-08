1c84de47ec4ff3f630db529ad369ccc8c16eef3e
gonghuachang <gonghuachang@hotmail.com>
2018-10-08 15:35:13 +0800

    templete.cc

---
+++ templete.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+ public:
+
+};
+
+
+int main() {
+  Solution sol = Solution();
+
+  return 0;
+}
