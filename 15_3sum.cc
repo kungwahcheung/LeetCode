3372082b2ec1947079e3c86c2ec9151ff87dc8ae
gonghuachang <gonghuachang@hotmail.com>
2018-10-09 23:43:38 +0800

    modify 15_3sum.cc

--- 15_3sum.cc
+++ 15_3sum.cc
@@ -14,7 +14,8 @@ class Solution {
   vector<vector<int>> threeSum(vector<int>& nums) {
     vector<vector<int>> res;
     sort(nums.begin(), nums.end());
-    for (int i = 0; i < nums.size(); ++i) {
+    int size = nums.size();
+    for (int i = 0; i < size - 2; ++i) {
       if (nums[i] > 0) break;
       if (i != 0 && nums[i] == nums[i-1]) continue;
       int left = i + 1, right = nums.size() - 1, target = 0 - nums[i];
