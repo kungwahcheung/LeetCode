7a610d022ed18f9a21f9d388942c3916ae38a3a5
wahcheung <gonghuachang@hotmail.com>
2018-12-18 10:52:26 +0800

    94_binary_tree_inorder_traversal.cc

--- 94_binary_tree_inorder_traversal.cc
+++ 94_binary_tree_inorder_traversal.cc
@@ -1,37 +1,60 @@
-#include <vector>
 #include <iostream>
+#include <stack>
+#include <vector>
 #include "treenode.h"
 
-using std::vector;
-using std::cout;
-using std::endl;
+#ifndef SOLUTION
+#define SOLUTION 2
+#endif  // SOLUTION
+
+using namespace std;
 
 class Solution {
  public:
+#if SOLUTION == 1
+  /* 递归求解 */
   vector<int> inorderTraversal(TreeNode* root) {
-    vector<int> res;
+    vector<int> res{};
     inorder(root, res);
     return res;
   }
- 
- private:
-  void inorder(TreeNode* root, vector<int>& traversal) {
+  void inorder(TreeNode* root, vector<int>& res) {
     if (!root) return;
-    inorder(root->left, traversal);
-    traversal.push_back(root->val);
-    inorder(root->right, traversal);
+    inorder(root->left, res);
+    res.push_back(root->val);
+    inorder(root->right, res);
   }
+
+#elif SOLUTION == 2
+  /* 非递归解法, 使用栈 */
+  vector<int> inorderTraversal(TreeNode* root) {
+    vector<int> res;
+    stack<TreeNode*> s;
+    TreeNode *p = root;
+    while (!s.empty() || p) {
+      if (p) {
+        s.push(p);
+        p = p->left;
+      } else {
+        p = s.top();
+        s.pop();
+        res.push_back(p->val);
+        p = p->right;
+      }
+    }
+    return res;
+  }
+#endif  // SOLUTION
 };
 
 
 int main() {
   Solution sol = Solution();
-  TreeNode *root = create_tree({1, None, 2, 3, 4});
-  print_tree(root);
-  vector<int> traversal = sol.inorderTraversal(root);
-  cout << endl;
-  for (auto it : traversal)
+  TreeNode *root = create_tree({1,None,3,2});
+  vector<int> res = sol.inorderTraversal(root);
+  for (auto it : res) {
     cout << it << '\t';
+  }
   cout << endl;
   return 0;
 }
